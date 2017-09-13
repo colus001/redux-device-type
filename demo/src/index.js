c2fe65352845c18e6ffa18633395033c33f1e467
Matthias Klan <matthias.klan@gmail.com>
2017-09-13 16:38:20 +0200

    Add files via upload

---
+++ demo/src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+
+import { deviceTypeReducer, initReduxDeviceType, EXTRA_SMALL, SMALL, MEDIUM, LARGE } from './redux-device-type';
+
+
+const reducers = combineReducers({
+  deviceType: deviceTypeReducer
+});
+
+const store = createStore(reducers);
+
+
+initReduxDeviceType(store);
+
+
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
+
+const App = ({ deviceType }) => (
+  <div style={styles}>
+    { deviceType <= SMALL && <div>SMALL AND LESS </div> }
+    { deviceType < EXTRA_SMALL && <div>SMALLER THAN EXTRA_SMALL</div>}
+    { deviceType === MEDIUM && <div>MEDIUM</div>}
+    { deviceType >= LARGE && <div>AT LEAST LARGE</div>}
+  </div>
+);
+
+const AppWithDeviceType = connect(
+  ({ deviceType }) => ({ deviceType }),
+  null
+)(App);
+
+render(<Provider store={store} >
+        <AppWithDeviceType />
+      </Provider>
+, document.getElementById('root'));
